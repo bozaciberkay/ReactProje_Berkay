bcc17ea2a685609cf5789fb5d78198998bf30a65
Admin Semih <admin.semih@poscodom.local>
2024-01-19 15:15:36 +0300

    viteconfig alanı degistirildi

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    port:3005,
+    host:"172.21.65.9"
+
+  },
   plugins: [react()],
 })
