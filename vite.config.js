19ddfa5113f5738676c967d6272966ac64e1a0b8
bozaciberkay <berkay.bozaci@poscoassan.com>
2024-01-30 14:06:26 +0300

    last versionn

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    port:3005,
-    host:"172.21.65.9"
+    port:3005
+    //host:"172.21.65.9"
 
   },
   plugins: [react()],
